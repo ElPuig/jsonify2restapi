7de633f823e2482ec32848048b8df4fcbcfdfacf
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2020-03-15 12:37:23 +0100

    Importa objetos con: type, title, id

--- jsonify2restapi.py
+++ jsonify2restapi.py
@@ -3,9 +3,13 @@ import requests
 import json
 import os
 
+from logzero import logger
+
 
 dir ="/home/vcarceler/desarrollo/jsonify2restapi/content-import"
 url = 'http://10.231.51.229:8080'
+plone_user = 'admin'
+plone_password = 'UUXdbVpOxgRf'
 
 # returns an integer from the filename
 def get_int(filename):
@@ -25,4 +29,31 @@ for d in sorted(os.listdir(dir), key=get_int):
 
         with open(filename, "r") as json_file:
             data = json.load(json_file)
-            print(" - Type: " + data["_type"])
+
+            new_data = {}
+            new_data['@type'] = data['_type']
+            new_data['title'] = data['title']
+            new_data['id'] = data['_path'][data['_path'].rfind('/')+1:]
+            logger.debug("id: " + new_data['id'])
+            # print(" - Type: " + data['_type'])
+
+            # set restapi @type
+            # data['@type'] = data['_type']
+            # data['@id'] = data['id']
+
+            # post data to restapi
+            #url_post = url + "/Plone"#+ data["_path"]
+            url_post = url + data['_path'][0:data['_path'].rfind('/')]
+            logger.debug("url_post: " + url_post)
+            r = requests.post(url_post,
+                headers={'Accept': 'application/json', 'Content-Type': 'application/json'},
+                json=new_data,
+                auth=(plone_user, plone_password))
+
+            if r.status_code != 201:
+                logger.error(str(r.status_code) + ":" + " id: " + new_data['id'] + " type:" + new_data['@type'] + " " + filename)
+                logger.error(new_data)
+                break
+            else:
+                print(r.text)
+                logger.info("Ok")
